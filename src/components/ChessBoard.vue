7cbd5b8248f5a0e7bb866d7817607c21061db408
davefollett <davefollett@gmail.com>
2018-05-17 15:45:33 -0400

    fixed row legend. misc changes.

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="grid">
-    <div v-for="columnName in columnLegend" :key="`top-${columnName}`">
+    <div v-for="(columnName,index) in columnLegend" :key="`top-${index}`">
       <ChessSquare :value="columnName" :hasBorder="false"></ChessSquare>
     </div>
     <div v-for="n in 80" :key="n">
-      <ChessSquare :value="n.toString()"></ChessSquare>
+      <ChessSquare v-if="n % 10 === 1 || n % 10 === 0" :value="rowLegend[n]" :hasBorder="false" ></ChessSquare>
+      <ChessSquare v-else :value="`${getRow(n)},${getColumn(n)}`"></ChessSquare>
     </div>
-    <div v-for="columnName in columnLegend" :key="`bottom-${columnName}`">
+    <div v-for="(columnName,index) in columnLegend" :key="`bottom-${index}`">
       <ChessSquare :value="columnName" :hasBorder="false"></ChessSquare>
     </div>
   </div>
 </template>
+
 
 <script>
 import ChessSquare from '@/components/ChessSquare.vue'
@@ -22,7 +24,33 @@ export default {
   },
   data () {
     return {
-      columnLegend: [" ", "a", "b", "c", "d", "e", "f", "g", "h", " "]
+      columnLegend: [" ", "a", "b", "c", "d", "e", "f", "g", "h", " "],
+      rowLegend: {
+        1: "8",
+        10: "8",
+        11: "7",
+        20: "7",
+        21: "6",
+        30: "6",
+        31: "5",
+        40: "5",
+        41: "4",
+        50: "4",
+        51: "3",
+        60: "3",
+        61: "2",
+        70: "2",
+        71: "1",
+        80: "1",
+      }
+    }
+  },
+  methods: {
+    getRow(cellNumber) {
+      return Math.floor(cellNumber / 10);
+    },
+    getColumn(cellNumber) {
+      return (cellNumber % 10) -2;
     }
   }
 }
@@ -33,5 +61,7 @@ export default {
     display: grid;
     grid-template-columns: repeat(10, 1fr);
     grid-gap: 2px;
+    align-items: center;
 }
+
 </style>
